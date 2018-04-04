72e85826ac69227d1b7d8e2c0dcaeaa1a9710940
Jameel Matin <jameel.matin@gmail.com>
2018-04-03 22:37:45 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var room = "dining room";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === 'ballroom') {
+    weapon = 'poison';  solved = suspect === 'Mr. Kalehoff';
+} else if (room === 'gallery') {
+    weapon = 'trophy';
+    solved = suspect === 'Ms. Van Cleve';
+} else if (room === 'billiards room') {
+    weapon = 'pool stick';
+    solved = suspect === 'Mrs. Sparr';
+} else {
+    weapon = 'knife';
+    solved = suspect === "Mr. Parkes";
+}
+
+if (solved) {
+        console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
